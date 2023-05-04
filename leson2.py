d1e6941f4b7e27b7e10e9129d2c3d7fddc433dba
Fixoregg <fixoreggff@gmail.com>
2023-05-04 20:51:46 +0300

    leson2

--- leson2.py
+++ leson2.py
@@ -4,12 +4,12 @@ class Student:
     def __init__(self, name):
         self.name = name
         self.gladness = 50
-        self.progress = 0
+        self.satiety = 0
         self.alive = True
 
-    def to_study(self):
-        print("Time to study")
-        self.progress += 0.12
+    def to_eating(self):
+        print("Time to eating")
+        self.satiety += 0.12
         self.gladness -= 3
 
 
@@ -20,29 +20,29 @@ class Student:
     def to_chill(self):
         print("Rest time")
         self.gladness += 5
-        self.progress -= 0.1
+        self.satiety -= 0.1
 
     def is_alive(self):
-        if self.progress < -0.5:
+        if self.satiety < -0.5:
             print("Cast out…")
             self.alive = False
         elif self.gladness <= 0:
             print("Depression…")
             self.alive = False
-        elif self.progress > 5:
+        elif self.satiety > 5:
             print("Passed externally…")
             self.alive = False
 
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
-        print(f"Progress = {round(self.progress, 2)}")
+        print(f"satiety = {round(self.satiety, 2)}")
 
     def live(self, day):
         day = "Day " + str(day) + " of " + self.name + " life"
         print(f"{day:=^50}")
         live_cube = random.randint(1, 3)
         if live_cube == 1:
-            self.to_study()
+            self.to_eating()
         elif live_cube == 2:
             self.to_sleep()
         elif live_cube == 3:
@@ -50,15 +50,15 @@ class Student:
         self.end_of_day()
         self.is_alive()
 
-nick = Student(name="Nick")
-kate = Student(name="Kate")
+Kat = Student(name="Kat")
+Dog = Student(name="Dog")
 for day in range(365):
-    if nick.alive == False:
+    if Kat.alive == False:
         break
-    nick.live(day)
-    if kate.alive == False:
+    Kat.live(day)
+    if Dog.alive == False:
         break
-    kate.live(day)
+    Dog.live(day)
 
 #
 #
